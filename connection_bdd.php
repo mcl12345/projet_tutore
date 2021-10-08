378ae78967af2ff2ceb2462836e1a6d96501403e
root <root@vps431735.ovh.net>
2021-10-08 19:04:33 +0200

    delete obsolete

--- connection_bdd.php
+++ connection_bdd.php
@@ -1,29 +1,10 @@
 <?php
 
 $db_host = '127.0.0.1';
-$db_name   = 'projet_tutore3';
+$db_name   = 'projet_tutore';
 $db_user = 'newuser';
-$db_password = 'password';
+$db_password = 'WMSqMIdCqc7tx653';
 $charset = 'utf8mb4';
 
-/*$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
-*/
-/*$stmt = $pdo->query('SELECT * FROM user');
-while ($row = $stmt->fetch())
-{
-    echo $row['username'] . " ";
-}
-echo "<br />";
-//print_r($stmt->fetchAll());
-*/
+
 ?>
